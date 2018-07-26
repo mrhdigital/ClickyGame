6102c84eaa89b57525f85984d735b53feb189b1f
unknown <mrhdigital@yahoo.com>
2018-07-25 21:18:19 -0500

    wrapper added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Wrapper from './components/Wrapper'
+
 import Title from './components/Title'
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper>
          <Title />
-      </div>
+         </Wrapper>
     );
   }
 }
